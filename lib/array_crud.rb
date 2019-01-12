cd9059f50334f0d3e96903fcbb413f3710d7acb6
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-12 23:45:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array ["John", "Mary", "Nancy"]
+  array ["John", "Mary", "Nancy", "Jim"]
 end
 
 def add_element_to_end_of_array(array, element)
